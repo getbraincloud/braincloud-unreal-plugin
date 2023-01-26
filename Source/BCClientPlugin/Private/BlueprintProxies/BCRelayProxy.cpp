9964b190e4155e7b9b438233f5b17f186183e477
franco <82105876+francobithead@users.noreply.github.com>
2023-01-26 11:31:09 -0500

    Revert "Moving the explicit disconnect in disconnect"
    This reverts commit 834d27c70c9a5ab8c05f77fddc4b4057370ff23f.

--- Source/BCClientPlugin/Private/BlueprintProxies/BCRelayProxy.cpp
+++ Source/BCClientPlugin/Private/BlueprintProxies/BCRelayProxy.cpp
@@ -23,6 +23,11 @@ void UBCRelayProxy::Disconnect(UBrainCloudWrapper *brainCloudWrapper)
 	UBCWrapperProxy::GetBrainCloudInstance(brainCloudWrapper)->getRelayService()->disconnect();
 }
 
+void UBCRelayProxy::ExplicitDisconnect(UBrainCloudWrapper* brainCloudWrapper)
+{
+	UBCWrapperProxy::GetBrainCloudInstance(brainCloudWrapper)->getRelayService()->explicitDisconnect();
+}
+
 bool UBCRelayProxy::IsConnected(UBrainCloudWrapper *brainCloudWrapper)
 {
 	return UBCWrapperProxy::GetBrainCloudInstance(brainCloudWrapper)->getRelayService()->isConnected();
