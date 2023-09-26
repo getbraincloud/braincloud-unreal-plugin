1b04661966a24987e07d02a7fb97ed21dcf3dd8b
Nick H <nickh@bitheads.com>
2023-09-26 16:59:26 -0400

    Adjusted widget as per Pauls feedback - Removed S2S Url and made the server url automatically remove the url path when committed - Changed GetBCAppData function to derive the main server URL and S2S url from the saved server url - Changed AppID to App ID in widget - Added function in function library to remove the path and validate URL strings

--- Source/BCClientPlugin/Private/BrainCloudFunctionLibrary.cpp
+++ Source/BCClientPlugin/Private/BrainCloudFunctionLibrary.cpp
@@ -2,6 +2,7 @@
 
 #include "BrainCloudFunctionLibrary.h"
 #include "BCClientPluginPrivatePCH.h"
+#include <Engine/Engine.h>
 
 FBrainCloudAppDataStruct UBrainCloudFunctionLibrary::GetBCAppData()
 {
@@ -36,12 +37,14 @@ FBrainCloudAppDataStruct UBrainCloudFunctionLibrary::GetBCAppData()
 
             ConfigSection->GenerateKeyArray(configKeys);
 
+            FString server = configKeys.Contains(*ServerUrlKey) ? ConfigSection->Find(*ServerUrlKey)->GetValue() : TEXT("");
+
             Result.AppId = configKeys.Contains(*AppIdKey) ? ConfigSection->Find(*AppIdKey)->GetValue() : TEXT("");
             Result.AppSecret = configKeys.Contains(*AppSecretKey) ? ConfigSection->Find(*AppSecretKey)->GetValue() : TEXT("");
             Result.Version = configKeys.Contains(*AppVersionKey) ? ConfigSection->Find(*AppVersionKey)->GetValue() : TEXT("");
-            Result.ServerUrl = configKeys.Contains(*ServerUrlKey) ? ConfigSection->Find(*ServerUrlKey)->GetValue() : TEXT("");
+            Result.ServerUrl = server + "/dispatcherv2";
             Result.S2SKey = configKeys.Contains(*S2SKeyKey) ? ConfigSection->Find(*S2SKeyKey)->GetValue() : TEXT("");
-            Result.S2SUrl = configKeys.Contains(*S2SUrlKey) ? ConfigSection->Find(*S2SUrlKey)->GetValue() : TEXT("");
+            Result.S2SUrl = server + "/s2sdispatcher";
         }
         else {
             UE_LOG(LogBrainCloud, Warning, TEXT("Couldn't find BrainCloudSettings.ini file in projects Config folder"));
@@ -77,9 +80,34 @@ void UBrainCloudFunctionLibrary::SetBCAppData(FBrainCloudAppDataStruct appData)
     GConfig->SetString(*SectionName, TEXT("Version"), *appData.Version, ConfigPath);
     GConfig->SetString(*SectionName, TEXT("ServerUrl"), *appData.ServerUrl, ConfigPath);
     GConfig->SetString(*SectionName, TEXT("S2SKey"), *appData.S2SKey, ConfigPath);
-    GConfig->SetString(*SectionName, TEXT("S2SUrl"), *appData.S2SUrl, ConfigPath);
 
     GConfig->Flush(false, ConfigPath);
 
     UE_LOG(LogBrainCloud, Warning, TEXT("App Data saved to config file, please restart Unreal Editor for changes to take effect"));
+}
+
+bool UBrainCloudFunctionLibrary::ValidateAndExtractURL(const FString& InputURL, FString& OutURL)
+{
+
+    if (InputURL.IsEmpty())
+        return false;
+
+
+    // Parse the input URL using FURL
+    UE_LOG(LogBrainCloud, Log, TEXT("In URL : %s"), *InputURL);
+    FURL ParsedURL(nullptr, *InputURL, TRAVEL_Absolute);
+    UE_LOG(LogBrainCloud, Log, TEXT("Parsed in URL : %s"), *ParsedURL.Host);
+    // Check if the URL is valid
+    if (ParsedURL.Valid)
+    {
+        // Construct the base URL without the path
+        OutURL = FString::Printf(TEXT("%s://%s"), *ParsedURL.Protocol, *ParsedURL.Host);
+        UE_LOG(LogBrainCloud, Log, TEXT("Out URL : %s"), *OutURL);
+        return true; // URL is valid
+    }
+    else
+    {
+        // URL is not valid
+        return false;
+    }
 }
