294fa0e63bad8103f4bafe9f3ef812540ce6a93f
Nick H <nickh@bitheads.com>
2024-04-29 15:15:50 -0400

    Added Android Native module to get country code as well as other useful functions to get device info

---
+++ Source/AndroidNative/Private/AndroidNativeLibrary.cpp
@@ -0,0 +1,97 @@
+// Georgy Treshchev 2024.
+
+#include "AndroidNativeLibrary.h"
+#include "AndroidNative.h"
+
+#include "AndroidNativeUtils.h"
+
+static const ANSICHAR* DeviceInfoClassName = "com/Plugins/AndroidNative/DeviceInfo";
+
+void UAndroidNativeLibrary::EnableNsdService(int32 InPort)
+{
+	AndroidNativeUtils::CallJavaStaticMethod<void>(DeviceInfoClassName, "startNsdService", FAndroidGameActivity(), InPort);
+}
+
+bool UAndroidNativeLibrary::IsInternetAvailable()
+{
+	return AndroidNativeUtils::CallJavaStaticMethod<bool>(DeviceInfoClassName, "IsInternetAvailable");
+}
+
+FString UAndroidNativeLibrary::GetGeoLocation()
+{
+	return AndroidNativeUtils::CallJavaStaticMethod<FString>(DeviceInfoClassName, "GetGeoLocation", FAndroidGameActivity());
+}
+
+FBaseDeviceInfo UAndroidNativeLibrary::GetBaseDeviceInfo()
+{
+	
+	FString UniqueID{AndroidNativeUtils::CallJavaStaticMethod<FString>(DeviceInfoClassName, "GetUniqueID", FAndroidGameActivity())};
+	FString OSVersion{AndroidNativeUtils::CallJavaStaticMethod<FString>(DeviceInfoClassName, "GetOSVersion")};
+	const int32 SDKVersion{AndroidNativeUtils::CallJavaStaticMethod<int32>(DeviceInfoClassName, "GetSDKVersion")};
+	FString Brand{AndroidNativeUtils::CallJavaStaticMethod<FString>(DeviceInfoClassName, "GetBrand")};
+	FString Model{AndroidNativeUtils::CallJavaStaticMethod<FString>(DeviceInfoClassName, "GetModel")};
+	FString Product{AndroidNativeUtils::CallJavaStaticMethod<FString>(DeviceInfoClassName, "GetProduct")};
+	FString Language{AndroidNativeUtils::CallJavaStaticMethod<FString>(DeviceInfoClassName, "GetLanguage")};
+	FString LanguageCode{AndroidNativeUtils::CallJavaStaticMethod<FString>(DeviceInfoClassName, "GetLanguageCode")};
+
+	FBaseDeviceInfo Result;
+	{
+		Result.UniqueID = MoveTemp(UniqueID);
+		Result.OSVersion = MoveTemp(OSVersion);
+		Result.SDKVersion = SDKVersion;
+		Result.Brand = MoveTemp(Brand);
+		Result.Model = MoveTemp(Model);
+		Result.Product = MoveTemp(Product);
+		Result.Language = MoveTemp(Language);
+		Result.LanguageCode = MoveTemp(LanguageCode);
+	}
+
+	return Result;
+}
+
+FString UAndroidNativeLibrary::GetCountryCode()
+{
+	FString countryCode = "";
+	countryCode = AndroidNativeUtils::CallJavaStaticMethod<FString>(DeviceInfoClassName, "GetCountryCodeFromSIM", FAndroidGameActivity());
+	UE_LOG(LogTemp, Log, TEXT("Got country code from SIM: %s"), *countryCode);
+	countryCode = AndroidNativeUtils::CallJavaStaticMethod<FString>(DeviceInfoClassName, "GetCountryCodeFromNetwork", FAndroidGameActivity());
+	UE_LOG(LogTemp, Log, TEXT("Got country code from Network: %s"), *countryCode);
+	countryCode = AndroidNativeUtils::CallJavaStaticMethod<FString>(DeviceInfoClassName, "GetCountryCodeFromLocale", FAndroidGameActivity());
+	UE_LOG(LogTemp, Log, TEXT("Got country code from Locale: %s"), *countryCode);
+
+	return countryCode;
+}
+
+FString UAndroidNativeLibrary::GetCountryCodeSIM()
+{
+	return AndroidNativeUtils::CallJavaStaticMethod<FString>(DeviceInfoClassName, "GetCountryCodeFromSIM", FAndroidGameActivity());
+}
+
+FString UAndroidNativeLibrary::GetCountryCodeNetwork()
+{
+	return AndroidNativeUtils::CallJavaStaticMethod<FString>(DeviceInfoClassName, "GetCountryCodeFromNetwork", FAndroidGameActivity());
+}
+
+FString UAndroidNativeLibrary::GetCountryCodeLocale()
+{
+	return AndroidNativeUtils::CallJavaStaticMethod<FString>(DeviceInfoClassName, "GetCountryCodeFromLocale", FAndroidGameActivity());
+}
+
+
+EAndroidTheme UAndroidNativeLibrary::GetCurrentSystemTheme()
+{
+	const uint8 Theme{AndroidNativeUtils::CallJavaStaticMethod<uint8>(DeviceInfoClassName, "GetCurrentSystemTheme", FAndroidGameActivity())};
+
+	switch (Theme)
+	{
+	case 0: return EAndroidTheme::Undefined;
+	case 1: return EAndroidTheme::Light;
+	case 2: return EAndroidTheme::Dark;
+	default: return EAndroidTheme::Undefined;
+	}
+}
+
+FString UAndroidNativeLibrary::GetExternalPath()
+{
+	return AndroidNativeUtils::CallJavaStaticMethod<FString>(DeviceInfoClassName, "GetExternalPath", FAndroidGameActivity());
+}
---
+++ Source/AndroidNative/Public/AndroidNativeUtils.h
@@ -0,0 +1,67 @@
+// Georgy Treshchev 2024.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "JavaConvert.h"
+
+#include "Helpers/StaticNativeCaller.h"
+#include "Helpers/SignatureHelper.h"
+#include "CustomJavaTypes.h"
+
+#include "AndroidNativeDefines.h"
+
+/**
+ * Helper for converting argument types from CPP to Java
+ */
+namespace ArgumentsConverter
+{
+	template <typename AnyType, TEnableIfNotCustomJavaArgument<AnyType> = true>
+	ANDROIDNATIVE_API AnyType FORCEINLINE ConvertArgument(const AnyType& Object) { return Object; }
+
+	template <typename AnyType, TEnableIfCustomJavaArgument<AnyType> = true>
+	ANDROIDNATIVE_API jobject FORCEINLINE ConvertArgument(const AnyType& CustomObject) { return CustomObject.GetValue(); }
+
+	ANDROIDNATIVE_API jboolean FORCEINLINE ConvertArgument(const bool Bool) { return AndroidNative_JavaConverter::ToJavaBool(Bool); }
+	ANDROIDNATIVE_API jbooleanArray FORCEINLINE ConvertArgument(const TArray<bool>& BoolArray) { return AndroidNative_JavaConverter::ToJavaBoolArray(BoolArray); }
+
+	ANDROIDNATIVE_API jbyte FORCEINLINE ConvertArgument(const uint8 Byte) { return AndroidNative_JavaConverter::ToJavaByte(Byte); }
+	ANDROIDNATIVE_API jbyteArray FORCEINLINE ConvertArgument(const TArray<uint8>& ByteArray) { return AndroidNative_JavaConverter::ToJavaByteArray(ByteArray); }
+
+	ANDROIDNATIVE_API jchar FORCEINLINE ConvertArgument(const UTF16CHAR Char) { return AndroidNative_JavaConverter::ToJavaChar(Char); }
+	ANDROIDNATIVE_API jcharArray FORCEINLINE ConvertArgument(const TArray<UTF16CHAR>& CharArray) { return AndroidNative_JavaConverter::ToJavaCharArray(CharArray); }
+
+	ANDROIDNATIVE_API jshort FORCEINLINE ConvertArgument(const short Short) { return AndroidNative_JavaConverter::ToJavaShort(Short); }
+	ANDROIDNATIVE_API jshortArray FORCEINLINE ConvertArgument(const TArray<short>& ShortArray) { return AndroidNative_JavaConverter::ToJavaShortArray(ShortArray); }
+
+	ANDROIDNATIVE_API jint FORCEINLINE ConvertArgument(const int32 Int) { return AndroidNative_JavaConverter::ToJavaInt(Int); }
+	ANDROIDNATIVE_API jintArray FORCEINLINE ConvertArgument(const TArray<int32>& IntArray) { return AndroidNative_JavaConverter::ToJavaIntArray(IntArray); }
+
+	ANDROIDNATIVE_API jlong FORCEINLINE ConvertArgument(const long Long) { return AndroidNative_JavaConverter::ToJavaLong(Long); }
+	ANDROIDNATIVE_API jlongArray FORCEINLINE ConvertArgument(const TArray<long>& LongArray) { return AndroidNative_JavaConverter::ToJavaLongArray(LongArray); }
+
+	ANDROIDNATIVE_API jfloat FORCEINLINE ConvertArgument(const float Float) { return AndroidNative_JavaConverter::ToJavaFloat(Float); }
+	ANDROIDNATIVE_API jfloatArray FORCEINLINE ConvertArgument(const TArray<float>& FloatArray) { return AndroidNative_JavaConverter::ToJavaFloatArray(FloatArray); }
+
+	ANDROIDNATIVE_API jdouble FORCEINLINE ConvertArgument(const double Double) { return AndroidNative_JavaConverter::ToJavaDouble(Double); }
+	ANDROIDNATIVE_API jdoubleArray FORCEINLINE ConvertArgument(const TArray<double>& DoubleArray) { return AndroidNative_JavaConverter::ToJavaDoubleArray(DoubleArray); }
+
+	ANDROIDNATIVE_API jstring FORCEINLINE ConvertArgument(const FString& String) { return AndroidNative_JavaConverter::ToJavaString(String); }
+	ANDROIDNATIVE_API jstring FORCEINLINE ConvertArgument(const TCHAR* String) { return AndroidNative_JavaConverter::ToJavaString(String); }
+	ANDROIDNATIVE_API jobjectArray FORCEINLINE ConvertArgument(const TArray<FString>& StringArray) { return AndroidNative_JavaConverter::ToJavaStringArray(StringArray); }
+}
+
+class ANDROIDNATIVE_API AndroidNativeUtils
+{
+public:
+	/**
+	 * Main function for calling Java Static Methods
+	 */
+	template <typename ReturnType, typename... Args>
+	static ReturnType CallJavaStaticMethod(const ANSICHAR* ClassName, const ANSICHAR* MethodName, Args ... args)
+	{
+		const FString MethodSignature{SignatureHelper::GetMethodSignature<ReturnType>(args...)};
+		return StaticNativeCaller::CallJavaStaticMethod<ReturnType>(ClassName, MethodName, TCHAR_TO_ANSI(*MethodSignature), ArgumentsConverter::ConvertArgument(args)...);
+	}
+};
