486db695b615ebcc4fb1ca7afbfdd9960078ff7f
Jo Hoar <joanneh@bitheads.com>
2024-05-06 12:23:54 -0400

    reverted version change

--- Source/BCClientPlugin/Private/BrainCloudClient.cpp
+++ Source/BCClientPlugin/Private/BrainCloudClient.cpp
@@ -23,7 +23,7 @@
 #include "Internationalization/Culture.h"
 
 // Define all static member variables.
-FString BrainCloudClient::s_brainCloudClientVersion = TEXT("5.2.2");
+FString BrainCloudClient::s_brainCloudClientVersion = TEXT("5.2.3");
 
 ////////////////////////////////////////////////////
 // (De)Constructors
