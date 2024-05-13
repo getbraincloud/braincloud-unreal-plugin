3dc72cb29518415a06db9ccea9252cb8e530c94e
Nick H <nickh@bitheads.com>
2024-05-13 16:14:29 -0400

    Incremented version to 5.3.0

--- Source/BCClientPlugin/Private/BrainCloudClient.cpp
+++ Source/BCClientPlugin/Private/BrainCloudClient.cpp
@@ -23,7 +23,7 @@
 #include "Internationalization/Culture.h"
 
 // Define all static member variables.
-FString BrainCloudClient::s_brainCloudClientVersion = TEXT("5.2.3");
+FString BrainCloudClient::s_brainCloudClientVersion = TEXT("5.3.0");
 
 ////////////////////////////////////////////////////
 // (De)Constructors
