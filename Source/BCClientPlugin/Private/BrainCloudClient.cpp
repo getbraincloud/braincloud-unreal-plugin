5f5aa21da9824078c0749f80d0716d3412537855
Franco Lago <82105876+francobithead@users.noreply.github.com>
2025-09-15 10:51:04 -0400

    Release/5.8.1 (#146)
    * Redid the braincloud login widget in UE5.0 version and fixed compile errors for UE5.0
    
    * Update to widget made in UE 5.0
    
    * Update client lib version to 5.8.1

--- Source/BCClientPlugin/Private/BrainCloudClient.cpp
+++ Source/BCClientPlugin/Private/BrainCloudClient.cpp
@@ -23,7 +23,7 @@
 #include "Internationalization/Culture.h"
 
 // Define all static member variables.
-FString BrainCloudClient::s_brainCloudClientVersion = TEXT("5.8");
+FString BrainCloudClient::s_brainCloudClientVersion = TEXT("5.8.1");
 
 ////////////////////////////////////////////////////
 // (De)Constructors
